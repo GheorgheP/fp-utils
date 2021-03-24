b0ac8f77dcae3303e9c4a71899de141de0ef49ae
Pisarenco Gheorghe <pisarenco.gh@gmail.com>
2021-03-24 08:59:26 +0200

    Added `isT` predicate

--- src/Nothing.ts
+++ src/Nothing.ts
@@ -9,6 +9,11 @@ export type MNothing<T> = T | Nothing;
 export const isNothing = (v: unknown): v is Nothing => v === null || v === undefined;
 
 /**
+ * Check whenever an potential maybe value is empty or not
+ */
+export const isT = <T>(t: MNothing<T>): t is T => !isNothing(t);
+
+/**
  * Return the default value if the provided value is Nothing
  */
 export const orElse = <T>(orElse: T, v: MNothing<T>): T => (isNothing(v) ? orElse : v);
--- src/__tests__/Nothing.ts
+++ src/__tests__/Nothing.ts
@@ -1,4 +1,4 @@
-import { isNothing, orElse } from "../Nothing";
+import { isNothing, orElse, isT } from "../Nothing";
 import { nothings, empties, values, prettify } from "../internals/utils";
 
 describe("Testing 'isNothing' function", () => {
@@ -9,6 +9,26 @@ describe("Testing 'isNothing' function", () => {
   [...empties, ...values].map((v) => {
     test(`If value is ${prettify(v)}, return false`, () => {
       expect(isNothing(v)).toBe(false);
+    });
+  });
+});
+
+describe("Testing 'isT' function", () => {
+  test.each(nothings)("If value is %s, return false", (v) => {
+    expect(isT(v)).toBe(false);
+  });
+
+  [...empties, ...values].map((v) => {
+    test(`If value is ${prettify(v)}, return true`, () => {
+      expect(isT(v)).toBe(true);
+    });
+  });
+
+  describe("isT is a negation of isNothing", () => {
+    [...nothings, ...empties, ...values].forEach((v) => {
+      test(`isNothing(${prettify(v)}) === !isT(${prettify(v)})`, () => {
+        expect(isNothing(v)).toBe(!isT(v));
+      });
     });
   });
 });
